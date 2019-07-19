4ab5afd2da53f053e2ade6614177c0263a03dadb
mateusz <mateusz_gil@outlook.com>
2019-07-19 11:45:45 +0200

    Removed functionf from class Parking which are responsible for reservations and tests for that

--- src/main/java/Traffic/Parking.java
+++ src/main/java/Traffic/Parking.java
@@ -27,7 +27,7 @@ public class Parking
     public String toString()
     {
         String out = "Parking Name: Default Name " + "\n"
-                + " places: " + this.occupiedPlaces + "/" + this.numberOfPlaces;
+                + " Places: " + this.occupiedPlaces + "/" + this.numberOfPlaces;
 
         for(int i = 0; i <this.numberOfPlaces; i++)
         {
@@ -40,29 +40,6 @@ public class Parking
         return out;
     }
 
-    public int reservePlace(int number_of_place)
-    {
-
-        if(this.inRange(number_of_place)) { // Does it exist ?
-            if(this.isReserved(number_of_place)) // If it is occupied
-            {
-                return 2;
-            }
-            else { // Reserving
-                this.occupiedPlaces += 1;
-                this.places[number_of_place] = true;
-            }
-
-
-            return 1;
-        }
-        else
-        {
-            throw new IllegalArgumentException("Invalid number of place");
-            //return 0;
-        }
-    }
-
     public boolean inRange(int number_of_place)
     {
         if(number_of_place < this.numberOfPlaces && number_of_place >= 0)
@@ -70,68 +47,6 @@ public class Parking
             return true;
         }
 
-        else
-            return false;
-    }
-
-
-    public boolean isReserved(int number_of_place)
-    {
-        if(this.places[number_of_place])
-        {
-            return true;
-        }
-
-        else
-            return false;
-    }
-
-    public int release(int number_of_place)
-    {
-
-        if (this.inRange(number_of_place)) { // Does exist that place ?
-            if (this.isFree(number_of_place))
-            { // Maybe it is already free
-                return 2;
-            }
-
-            else //Releasing
-                {
-                this.occupiedPlaces -= 1;
-                this.places[number_of_place] = false;
-                return 1;
-                }
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    public boolean isFree(int number_of_place)
-    {
-        if(this.places[number_of_place])
-        {
-            return false;
-        }
-
-        else
-            return true;
-    }
-
-    public void reset()
-    {
-        for(int i = 0; i<this.numberOfPlaces; i++)
-        {
-            this.places[i] = false;
-        }
-        this.occupiedPlaces = 0;
-    }
-
-    public boolean empty()
-    {
-        if(this.occupiedPlaces == 0)
-            return true;
         else
             return false;
     }
